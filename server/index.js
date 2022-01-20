62a6c0c4d4fcb4da48eacaab04c97a2cafe09ef8
Stephen Huang <huangstephen9@gmail.com>
2022-01-19 22:10:57 -0500

    Updaet PORT

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./db');
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 app.use(cors());
 
